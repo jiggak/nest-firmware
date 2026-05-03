8a8ea581eed78224fae245c10570110e1a583440
Josh Kropf <josh@slashdev.ca>
2026-05-03 17:51:41 -0400

    Replace connman-legacy with nest modified version

---
+++ buildroot/package/connman-nest/connman-nest.mk
@@ -0,0 +1,30 @@
+CONNMAN_NEST_VERSION = ba669b5831fd37dd868237c13db163256d1b0c52
+CONNMAN_NEST_SITE = https://nest-open-source.googlesource.com/nest-learning-thermostat/5.9.4/connman
+CONNMAN_NEST_SITE_METHOD = git
+CONNMAN_NEST_DEPENDENCIES = libglib2 dbus iptables-legacy readline
+CONNMAN_NEST_INSTALL_STAGING = YES
+CONNMAN_NEST_LICENSE = GPLv2
+CONNMAN_NEST_LICENSE_FILES = COPYING
+CONNMAN_NEST_CONF_OPTS += \
+	--with-dbusconfdir=/etc \
+	--enable-sleep \
+	--disable-loopback \
+	--disable-bluetooth \
+	--disable-ofono \
+	--disable-dundee \
+	--disable-pacrunner \
+	--disable-neard \
+	--disable-wispr \
+	--disable-tools
+
+define CONNMAN_NEST_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/connman/S45connman $(TARGET_DIR)/etc/init.d/S45connman
+endef
+
+define CONNMAN_NEST_INSTALL_CM
+	$(INSTALL) -m 0755 -D $(@D)/client/connmanctl $(TARGET_DIR)/usr/bin/connmanctl
+endef
+
+CONNMAN_NEST_POST_INSTALL_TARGET_HOOKS += CONNMAN_NEST_INSTALL_CM
+
+$(eval $(autotools-package))
